9a7a21d7ae9c9472197373e146412678c437f5d3
Abdurrahim Durgun <durgunabdurrahim@gmail.com>
2023-02-26 18:50:09 +0300

    python learning, first action

---
+++ firstapp.py
@@ -0,0 +1,21 @@
+#dik üçgende öklid
+
+# ucgenKenar1 = int(input('üçgenin alt kenarı : '))
+# ucgenKenar2 = int(input('üçgenin dik kenarı: '))
+# uzunKenar = (ucgenKenar1*ucgenKenar1) + (ucgenKenar2*ucgenKenar2)
+# ucgenKenar3 = uzunKenar**0.5
+# ucgenKenar3 = int(ucgenKenar3)
+# greeting ='en uzun kenar: '+  str(ucgenKenar3)
+# print(greeting[-1])
+
+
+
+# course = "python kursu: Baştan sona python programlama rehberiniz (40 saat)"
+# # print(len(course)) #65
+# website = "http://www.sadikturan.com"
+# newstr = website.replace("com","")
+# print(newstr)
+# print(course[50:65])
+# print(course[::-1])
+# name , surname , age , job = 'bora' , 'yılmaz' , '32' , 'mühendis'
+# print("benim adım {} {} , yaşım {} ve mesleğim {}".format(surname,name,job,age))
